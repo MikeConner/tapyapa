e036052b429bab7eeb6bc1a82c057264864dd96b
arash <arash.danaie@gmail.com>
2014-11-22 09:38:26 -0500

    Rails App - 3 - empty

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tapyapa::Application.config.secret_token = '4bb7834cb7d169eb2d0c7f29f3b7ffc5f87a075bcc5b2fc4d422b1b6d76e299659c6639776f24783a1d5fbdd2a0387fe4abdad322eb9c226e2d3093b39ffe1fc'
